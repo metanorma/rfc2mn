64ef9a82cde99ad5f542e8f30b4e0b613fb751dc
Alexande B <abobrikovich@gmail.com>
2019-07-18 19:52:58 +0300

    Add missing external references + download logic

--- test.sh
+++ test.sh
@@ -1,11 +1,16 @@
 #!/bin/env bash
 
+
+REFS=$(xmllint --xpath "/*[local-name()='catalog']/*[local-name()='group']/*[local-name()='system']/@systemId" external/sgml_catalogue_files.xml.in | sed 's/systemId=//g' | sed 's/"//g')
+pushd external
+for xml_url in "$REFS"; do wget -N $xml_url; done
+popd
+
 TEST_DIR=test
 XMLS="https://tools.ietf.org/id/draft-ribose-asciirfc-08.xml https://tools.ietf.org/id/draft-asciirfc-minimal-03.xml"
-
 mkdir -p $TEST_DIR
 pushd $TEST_DIR
-for xml_url in $XMLS; do wget $xml_url; done
+for xml_url in $XMLS; do wget -N $xml_url; done
 popd
 
 for xml in $TEST_DIR/*.xml; do ./rfc2mnadoc.sh $xml ${xml}.adoc; done
